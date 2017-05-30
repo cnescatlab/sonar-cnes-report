6d68a589c9554bb7c3e65a6d35aefcaa5508ebeb
labo <labo>
2017-05-30 15:08:37 +0200

    maven build + comments + refactoring + log4j

--- src/test/java/DocXExporterTest.java
+++ src/test/java/DocXExporterTest.java
@@ -1,17 +1,14 @@
-import fr.cnes.sonar.report.exporters.docx.DocXExporter;
-import org.junit.Test;
-
 /**
  * Test the creation of a docx file from a template
  * @author garconb
  */
 public class DocXExporterTest extends MasterTest {
 
-    @Test
-    public void variousTries() throws Exception {
-        DocXExporter de = new DocXExporter();
-
-        de.export(report, params, "analysis-report.docx");
-    }
+//    @Test
+//    public void variousTries() throws Exception {
+//        DocXExporter de = new DocXExporter();
+//
+//        de.export(report, params, "analysis-report.docx");
+//    }
 
 }
