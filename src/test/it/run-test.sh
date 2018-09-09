e766e2ec94fb8d871a733218d903a207b717210d
Benoît <benoit.garcon@outlook.com>
2018-09-09 23:36:14 +0200

    Correct itests

--- src/test/it/run-test.sh
+++ src/test/it/run-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-cp src/resources/template/*template* target
+cp src/main/resources/template/code-analysis-template.docx src/main/resources/template/issues-template.xlsx target
 cd target
 java -jar sonar-cnes-report.jar --sonar.project.id cnesreport --sonar.url http://localhost:9000
