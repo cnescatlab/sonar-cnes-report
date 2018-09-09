de82605e28eb030ebe215c1c1e0a6edcb974105d
Benoît Garçon <begarco@users.noreply.github.com>
2018-09-10 00:07:20 +0200

    Update run-test.sh

--- src/test/it/run-test.sh
+++ src/test/it/run-test.sh
@@ -2,4 +2,4 @@
 
 cp src/main/resources/template/code-analysis-template.docx src/main/resources/template/issues-template.xlsx target
 cd target
-java -jar sonar-cnes-report.jar --sonar.project.id cnesreport --sonar.url http://localhost:9000
+java -jar sonar-cnes-report.jar --sonar.project.id fr.cnes.sonar:sonar-cnes-report --sonar.url http://localhost:9000
