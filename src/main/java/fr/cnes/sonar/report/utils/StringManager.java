40e088a2fbb0a93123d55d5ccd519a493e40d8b1
John Ballard <j_ballard@tcdi.com>
2018-11-14 17:22:28 -0500

    Use the locale set on the command line.
    The change takes in consideration the --language set on the command
    line.

--- src/main/java/fr/cnes/sonar/report/utils/StringManager.java
+++ src/main/java/fr/cnes/sonar/report/utils/StringManager.java
@@ -32,44 +32,78 @@ import java.util.logging.Logger;
  */
 public final class StringManager {
 
-    /** Just an empty string. */
+    /**
+     * Just an empty string.
+     */
     public static final String EMPTY = "";
-    /** Just a tabulation. */
+    /**
+     * Just a tabulation.
+     */
     public static final String TAB = "\t";
-    /** Just a space. */
+    /**
+     * Just a space.
+     */
     public static final String SPACE = " ";
-    /** Just a space for URI. */
+    /**
+     * Just a space for URI.
+     */
     public static final String URI_SPACE = "%20";
-    /** Name for properties' file about report. */
+    /**
+     * Name for properties' file about report.
+     */
     public static final String REPORT_PROPERTIES = "report.properties";
-    /** Name of the property giving the server server. */
+    /**
+     * Name of the property giving the server server.
+     */
     public static final String SONAR_URL = "sonar.url";
-    /** Name of the property giving the token to authenticate to SonarQube. */
+    /**
+     * Name of the property giving the token to authenticate to SonarQube.
+     */
     public static final String SONAR_TOKEN = "sonar.token";
-    /** Logged message when there are too much issues to export. */
+    /**
+     * Logged message when there are too much issues to export.
+     */
     public static final String ISSUES_OVERFLOW_MSG = "log.overflow.msg";
-    /** Pattern to format the date. */
+    /**
+     * Pattern to format the date.
+     */
     public static final String DATE_PATTERN = "yyyy-MM-dd";
-    /** Default path to the target diretory for report files. */
+    /**
+     * Default path to the target diretory for report files.
+     */
     public static final String DEFAULT_OUTPUT = "report.path";
-    /** Default language for the report. */
+    /**
+     * Default language for the report.
+     */
     public static final String DEFAULT_LANGUAGE = "report.locale";
-    /** Default name for the author. */
+    /**
+     * Default name for the author.
+     */
     public static final String DEFAULT_AUTHOR = "report.author";
 
-    /** Logger for StringManager. */
+    /**
+     * Logger for StringManager.
+     */
     private static final Logger LOGGER = Logger.getLogger(StringManager.class.getCanonicalName());
-    
-    /** Contain all the properties related to the report. */
+
+    /**
+     * Contain all the properties related to the report.
+     */
     private static Properties properties;
 
-    /** Contains internationalized fields. */
+    /**
+     * Contains internationalized fields.
+     */
     private static ResourceBundle messages;
 
-    /** Define the language of the program. */
+    /**
+     * Define the language of the program.
+     */
     private static Locale currentLocale;
 
-    /** Unique instance of this class (singleton). */
+    /**
+     * Unique instance of this class (singleton).
+     */
     private static StringManager ourInstance = null;
 
     //
@@ -86,14 +120,14 @@ public final class StringManager {
         try {
             // load properties file as a stream
             input = classLoader.getResourceAsStream(StringManager.REPORT_PROPERTIES);
-            if(input!=null) {
+            if (input != null) {
                 // load properties from the stream in an adapted structure
                 properties.load(input);
             }
         } catch (IOException e) {
             LOGGER.log(Level.SEVERE, e.getMessage(), e);
         } finally {
-            if(input!=null) {
+            if (input != null) {
                 try {
                     // close the stream if necessary (not null)
                     input.close();
@@ -103,14 +137,16 @@ public final class StringManager {
             }
         }
 
+        String[] locale = properties.getProperty(StringManager.DEFAULT_LANGUAGE).split("_");
         // load internationalized strings, french by default
-        changeLocale("fr","FR");
+        changeLocale(locale[0], locale[1]);
     }
 
     /**
      * Private constructor to singletonize the class.
      */
-    private StringManager() {}
+    private StringManager() {
+    }
 
     /**
      * Get the singleton
@@ -127,6 +163,7 @@ public final class StringManager {
     /**
      * Give the value of the property corresponding to the key passed as parameter.
      * It gives only properties related to the report.
+     *
      * @param property Key of the property you want.
      * @return The value of the property you want as a String.
      */
@@ -136,18 +173,20 @@ public final class StringManager {
 
     /**
      * Change the locale and reload messages
+     *
      * @param language String in lowercase
-     * @param country String in upper case
+     * @param country  String in upper case
      */
     public static synchronized void changeLocale(final String language, final String country) {
         // change locale
-        currentLocale = new Locale(language,country);
+        currentLocale = new Locale(language, country);
         // reload messages
         messages = ResourceBundle.getBundle("messages", currentLocale);
     }
 
     /**
      * Return string corresponding to the given key according the locale
+     *
      * @param key name of the property in the bundle messages
      * @return a String
      */
