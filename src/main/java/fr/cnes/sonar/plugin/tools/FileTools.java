0a0eca24736d8f2d6dbac9323c61f739bacbd5b7
Louis MARTIN <louis.jd.martin@gmail.com>
2019-05-02 10:52:45 +0200

    reorganise and clean code

--- src/main/java/fr/cnes/sonar/plugin/tools/FileTools.java
+++ src/main/java/fr/cnes/sonar/plugin/tools/FileTools.java
@@ -1,9 +1,33 @@
+/*
+ * This file is part of cnesreport.
+ *
+ * cnesreport is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * cnesreport is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with cnesreport.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package fr.cnes.sonar.plugin.tools;
 
 
 import java.io.File;
 
 public class FileTools {
+
+    /**
+     * Delete a folder
+     * WARNING: Recursive call, maybe it's not a good idea for large folder with lots of subfolder.
+     * (created to clean the output folder when report is generated with sonarqube plugin).
+     * @param folder
+     */
     public static void deleteFolder(File folder){
         String[] files =  folder.list();
         File toDelete;
--- src/main/java/fr/cnes/sonar/plugin/ws/ReportWs.java
+++ src/main/java/fr/cnes/sonar/plugin/ws/ReportWs.java
@@ -1,29 +1,73 @@
+/*
+ * This file is part of cnesreport.
+ *
+ * cnesreport is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * cnesreport is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with cnesreport.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package fr.cnes.sonar.plugin.ws;
 
+import fr.cnes.sonar.plugin.PluginStringManager;
+import org.sonar.api.config.Configuration;
 import org.sonar.api.server.ws.WebService;
 
 public class ReportWs implements WebService {
+
+    // Sonarqube configuration
+    private final Configuration config;
+
+    /**
+     * public constructor, called by sonarqube
+     * @param config
+     */
+    public ReportWs(Configuration config){
+        this.config = config;
+    }
+
+    /**
+     * Define plugin, called at sonarqube startup
+     * @param context
+     */
     @Override
     public void define(Context context) {
-        final NewController controller = context.createController("api/cnesreport");
-        controller.setSince("6.7");
-        controller.setDescription("Blablabla");
+        final NewController controller = context.createController(PluginStringManager.getProperty("api.url"));
+        controller.setSince(PluginStringManager.getProperty("plugin.since"));
+        controller.setDescription(PluginStringManager.getProperty("api.description"));
         reportAction(controller);
         controller.done();
     }
 
-    public void reportAction(final WebService.NewController controller){
-        final WebService.NewAction report = controller.createAction("report");
-        report.setDescription("Blablabla");
-        report.setSince("7.4");
-        report.setHandler(new exportTask());
+    /**
+     * Define action executed when we called the webservice
+     * @param controller
+     */
+    private void reportAction(final WebService.NewController controller){
+        // Create API entry point
+        final WebService.NewAction report = controller.createAction(PluginStringManager.getProperty("api.report.actionKey"));
+        report.setDescription(PluginStringManager.getProperty("api.description"));
+        report.setSince(PluginStringManager.getProperty("plugin.since"));
 
-        WebService.NewParam keyParam = report.createParam("key");
-        keyParam.setDescription("Key of the project");
+        // Bind webservice to export task
+        report.setHandler(new exportTask(config));
+
+        // Adding key argument
+        WebService.NewParam keyParam = report.createParam(PluginStringManager.getProperty("api.report.args.key"));
+        keyParam.setDescription(PluginStringManager.getProperty("api.report.args.description.key"));
         keyParam.setRequired(true);
 
-        WebService.NewParam authorParam = report.createParam("author");
-        authorParam.setDescription("Author of the report");
+        // Adding author argument
+        WebService.NewParam authorParam = report.createParam(PluginStringManager.getProperty("api.report.args.author"));
+        authorParam.setDescription(PluginStringManager.getProperty("api.report.args.description.author"));
         authorParam.setRequired(true);
 
     }
