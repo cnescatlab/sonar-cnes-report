0ef549016e367f9530be2bac86865d7401530c3e
Louis MARTIN <louis.jd.martin@gmail.com>
2019-05-06 10:36:51 +0200

    FIX: Wrong url ic CI & undetected error

--- src/test/it/run-test.sh
+++ src/test/it/run-test.sh
@@ -3,4 +3,3 @@
 cp src/main/resources/template/code-analysis-template.docx src/main/resources/template/issues-template.xlsx target
 cd target
 java -jar cnesreport.jar -p fr.cnes.sonar:cnesreport -s http://localhost:9000
-wget http://127.0.0.1:9000/api/cnesreport/report?key=fr.cnes.sonar%cnesreport&author=travis-ci&token=noauth
