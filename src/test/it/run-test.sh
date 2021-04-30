c8ef19e35d1cb4108d6032481c9394b96212e36e
ErwanGauduchon <49336970+ErwanGauduchon@users.noreply.github.com>
2021-04-30 12:26:40 +0000

    add token to run-test.sh

--- src/test/it/run-test.sh
+++ src/test/it/run-test.sh
@@ -2,4 +2,4 @@
 
 cp src/main/resources/template/code-analysis-template.docx src/main/resources/template/issues-template.xlsx target
 cd target
-java -jar sonar-cnes-report.jar -p fr.cnes.sonar:cnesreport -s http://localhost:9000
+java -jar sonar-cnes-report.jar -t ${token_sonarqube} -p fr.cnes.sonar:cnesreport -s http://localhost:9000
