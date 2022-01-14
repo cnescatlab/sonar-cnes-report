602434697168fdb29ea918bc7a9dbcf625450970
Alexis Chatillon <46956548+Sancretor@users.noreply.github.com>
2022-01-14 16:52:37 +0100

    Refactor Docx Exporter

--- src/main/java/fr/cnes/sonar/report/exporters/docx/DocXExporter.java
+++ src/main/java/fr/cnes/sonar/report/exporters/docx/DocXExporter.java
@@ -54,7 +54,8 @@ public class DocXExporter implements IExporter {
      */
     private static final String SECURITY_HOTSPOTS_DETAILS_PLACEHOLDER = "$SECURITY_HOTSPOTS_DETAILS";
     /**
-     * Placeholder for the table containing counts of security hotspots by review priority and security category
+     * Placeholder for the table containing counts of security hotspots by review
+     * priority and security category
      */
     private static final String SECURITY_HOTSPOTS_COUNT_TABLE_PLACEHOLDER = "$SECURITY_HOTSPOTS_COUNT";
     /**
@@ -62,7 +63,8 @@ public class DocXExporter implements IExporter {
      */
     private static final String VOLUME_TABLE_PLACEHOLDER = "$VOLUME";
     /**
-     * Placeholder for the table containing values of each metric of the quality gate
+     * Placeholder for the table containing values of each metric of the quality
+     * gate
      */
     private static final String QUALITY_GATE_STATUS_TABLE_PLACEHOLDER = "$QUALITY_GATE_STATUS";
     /**
@@ -80,14 +82,15 @@ public class DocXExporter implements IExporter {
 
     /**
      * Overridden export for docX
-     * @param data Data to export as Report
-     * @param path Path where to export the file
+     * 
+     * @param data     Data to export as Report
+     * @param path     Path where to export the file
      * @param filename Name of the template file
      * @return Exported file.
      * @throws BadExportationDataTypeException Data has not the good type
-     * @throws OpenXML4JException ...
-     * @throws IOException ...
-     * @throws XmlException ...
+     * @throws OpenXML4JException              ...
+     * @throws IOException                     ...
+     * @throws XmlException                    ...
      */
     @Override
     public File export(final Object data, final String path, final String filename)
@@ -103,83 +106,46 @@ public class DocXExporter implements IExporter {
         final File file = new File(filename);
 
         // Check if template file exists
-        if(!file.exists() && !filename.isEmpty()) {
-            LOGGER.log(Level.WARNING, () -> "Unable to find provided DOCX template file (using default one instead) : " + file.getAbsolutePath());
+        if (!file.exists() && !filename.isEmpty()) {
+            LOGGER.log(Level.WARNING, () -> "Unable to find provided DOCX template file (using default one instead) : "
+                    + file.getAbsolutePath());
         }
 
         try (
-            InputStream fileInputStream = file.exists() ?
-                    new FileInputStream(file) : getClass().getResourceAsStream(StringManager.getProperty(DEFAULT_TEMPLATE));
-            OPCPackage opcPackage = OPCPackage.open(fileInputStream);
-            XWPFDocument document = new XWPFDocument(opcPackage)
-        ) {
+                InputStream fileInputStream = file.exists() ? new FileInputStream(file)
+                        : getClass().getResourceAsStream(StringManager.getProperty(DEFAULT_TEMPLATE));
+                OPCPackage opcPackage = OPCPackage.open(fileInputStream);
+                XWPFDocument document = new XWPFDocument(opcPackage)) {
 
             // Fill charts
             DocXTools.fillCharts(document, report.getFacets(), report.getTimeFacets());
 
             // Add issues
-            final List<List<String>> issues = DataAdapter.getIssues(report);
-            final String[] issuesArrayFr = {StringManager.string("header.name"),
-                StringManager.string("header.description"),
-                StringManager.string("header.type"),
-                StringManager.string("header.severity"),
-                StringManager.string(HEADER_NUMBER)};
-            final List<String> headerIssues = new ArrayList<>(Arrays.asList(issuesArrayFr));
-            DocXTools.fillTable(document, headerIssues, issues, DETAILS_TABLE_PLACEHOLDER);
+            replaceIssuesPlaceholders(document, report);
 
             // Add issues count by type and severity
-            final List<List<String>> types = DataAdapter.getTypes(report);
-            final List<String> headerIssuesCount = DataAdapter.getReversedIssuesSeverities();
-            headerIssuesCount.add(0, StringManager.string("header.typeSlashSeverity"));
-            DocXTools.fillTable(document, headerIssuesCount, types, COUNT_TABLE_PLACEHOLDER);
+            replaceIssuesCountPlaceholders(document, report);
 
             // Add security hotspots
-            final List<List<String>> securityHotspots = DataAdapter.getSecurityHotspots(report);
-            final String[] securityHotspotsHeader = {StringManager.string("header.category"),
-                StringManager.string("header.name"),
-                StringManager.string("header.priority"),
-                StringManager.string("header.severity"),
-                StringManager.string("header.count")};
-            final List<String> headerSecurityHotspots = new ArrayList<>(Arrays.asList(securityHotspotsHeader));
-            DocXTools.fillTable(document, headerSecurityHotspots, securityHotspots, SECURITY_HOTSPOTS_DETAILS_PLACEHOLDER);
+            replaceSecurityHotspotsPlaceholders(document, report);
 
-            // Add security hotspots by security category and review priority
-            final List<List<String>> securityHotspotsByCategoryAndPriority = DataAdapter.getSecurityHotspotsByCategoryAndPriority(report);
-            final List<String> headerSecurityHotspotsCount = new ArrayList<>(Arrays.asList(DataAdapter.getSecurityHotspotPriorities()));
-            headerSecurityHotspotsCount.add(0, StringManager.string("header.categorySlashPriority"));
-            DocXTools.fillTable(document, headerSecurityHotspotsCount, securityHotspotsByCategoryAndPriority,
-                    SECURITY_HOTSPOTS_COUNT_TABLE_PLACEHOLDER);
+            // Add security hotspots by security category and review priority.
+            replaceSecurityHotspotsCountPlaceholders(document, report);
 
             // Add volumes by language
-            final String[] volumesHeader = {StringManager.string("header.language"),
-                StringManager.string(HEADER_NUMBER)};
-            final List<String> headerVolumes = new ArrayList<>(Arrays.asList(volumesHeader));
-            final List<List<String>> volumes = DataAdapter.getVolumes(report);
-            DocXTools.fillTable(document, headerVolumes, volumes, VOLUME_TABLE_PLACEHOLDER);
+            replaceVolumesPlaceholders(document, report);
 
             // Add quality gate status
-            final String[] qualityGateStatusHeader = {StringManager.string("header.metric"),
-                StringManager.string("header.value")};
-            final List<String> headerQualityGateStatus = new ArrayList<>(Arrays.asList(qualityGateStatusHeader));
-            final List<List<String>> qualityGateStatus = DataAdapter.getQualityGateStatus(report);
-            DocXTools.fillTable(document, headerQualityGateStatus, qualityGateStatus, QUALITY_GATE_STATUS_TABLE_PLACEHOLDER);
+            replaceQualityGateStatusPlaceholders(document, report);
 
             // Add detailed technical debt
-            final String[] detailedTechnicalDebtHeader = {StringManager.string("header.reliability"),
-                StringManager.string("header.security"), StringManager.string("header.maintainability"),
-                StringManager.string("header.total")};
-            final List<String> headerDetailedTechnicalDebt = new ArrayList<>(Arrays.asList(detailedTechnicalDebtHeader));
-            final List<List<String>> detailedTechnicalDebt = DataAdapter.getDetailedTechnicalDebt(report);
-            DocXTools.fillTable(document, headerDetailedTechnicalDebt, detailedTechnicalDebt, DETAILED_TECHNICAL_DEBT_TABLE_PLACEHOLDER);
+            replaceDetailedTechnicalDebtPlaceholders(document, report);
 
-            // Map which contains all values to replace
-            // the key is the placeholder and the value is the value to write over
-            final Map<String, String> replacementValues = DataAdapter.loadPlaceholdersMap(report);
+            // replace simple placeholders (not tables/charts)
+            replaceSimplePlaceholders(document, report);
 
-            // replace all placeholder in the document (head, body, foot) with the map
-            DocXTools.replacePlaceholder(document, replacementValues);
-
-            // Save the result by creating a new file in the directory given by report.path property
+            // Save the result by creating a new file in the directory given by report.path
+            // property
             final FileOutputStream out = new FileOutputStream(path);
             // close open resources
             document.write(out);
@@ -187,6 +153,131 @@ public class DocXExporter implements IExporter {
         }
 
         return new File(path);
+    }
+
+    /**
+     * Replace simple placeholders with data
+     * 
+     * @param doc    The document containing placeholders
+     * @param report The report containing data
+     */
+    private void replaceSimplePlaceholders(XWPFDocument document, Report report) throws IOException, OpenXML4JException {
+        // Map which contains all values to replace
+        // the key is the placeholder and the value is the value to write over
+        final Map<String, String> replacementValues = DataAdapter.loadPlaceholdersMap(report);
+
+        // replace all placeholder in the document (head, body, foot) with the map
+        DocXTools.replacePlaceholder(document, replacementValues);
+    }
+
+    /**
+     * Replace issues placeholders with data
+     * 
+     * @param doc    The document containing placeholders
+     * @param report The report containing data
+     */
+    private void replaceIssuesPlaceholders(XWPFDocument document, Report report) {
+        final List<List<String>> issues = DataAdapter.getIssues(report);
+        final String[] issuesArrayFr = { StringManager.string("header.name"),
+                StringManager.string("header.description"),
+                StringManager.string("header.type"),
+                StringManager.string("header.severity"),
+                StringManager.string(HEADER_NUMBER) };
+        final List<String> headerIssues = new ArrayList<>(Arrays.asList(issuesArrayFr));
+        DocXTools.fillTable(document, headerIssues, issues, DETAILS_TABLE_PLACEHOLDER);
+    }
+
+    /**
+     * Replace issues count placeholders with data
+     * 
+     * @param doc    The document containing placeholders
+     * @param report The report containing data
+     */
+    private void replaceIssuesCountPlaceholders(XWPFDocument document, Report report) {
+        final List<List<String>> types = DataAdapter.getTypes(report);
+        final List<String> headerIssuesCount = DataAdapter.getReversedIssuesSeverities();
+        headerIssuesCount.add(0, StringManager.string("header.typeSlashSeverity"));
+        DocXTools.fillTable(document, headerIssuesCount, types, COUNT_TABLE_PLACEHOLDER);
+    }
+
+    /**
+     * Replace security hotspots count placeholders with data
+     * 
+     * @param doc    The document containing placeholders
+     * @param report The report containing data
+     */
+    private void replaceSecurityHotspotsPlaceholders(XWPFDocument document, Report report) {
+        final List<List<String>> securityHotspots = DataAdapter.getSecurityHotspots(report);
+        final String[] securityHotspotsHeader = { StringManager.string("header.category"),
+                StringManager.string("header.name"),
+                StringManager.string("header.priority"),
+                StringManager.string("header.severity"),
+                StringManager.string("header.count") };
+        final List<String> headerSecurityHotspots = new ArrayList<>(Arrays.asList(securityHotspotsHeader));
+        DocXTools.fillTable(document, headerSecurityHotspots, securityHotspots,
+                SECURITY_HOTSPOTS_DETAILS_PLACEHOLDER);
+    }
+
+    /**
+     * Replace security hotspots count placeholders with data
+     * 
+     * @param doc    The document containing placeholders
+     * @param report The report containing data
+     */
+    private void replaceSecurityHotspotsCountPlaceholders(XWPFDocument document, Report report) {
+        final List<List<String>> securityHotspotsByCategoryAndPriority = DataAdapter
+                .getSecurityHotspotsByCategoryAndPriority(report);
+        final List<String> headerSecurityHotspotsCount = new ArrayList<>(
+                Arrays.asList(DataAdapter.getSecurityHotspotPriorities()));
+        headerSecurityHotspotsCount.add(0, StringManager.string("header.categorySlashPriority"));
+        DocXTools.fillTable(document, headerSecurityHotspotsCount, securityHotspotsByCategoryAndPriority,
+                SECURITY_HOTSPOTS_COUNT_TABLE_PLACEHOLDER);
+    }
+
+    /**
+     * Replace volumes placeholders with data
+     * 
+     * @param doc    The document containing placeholders
+     * @param report The report containing data
+     */
+    private void replaceVolumesPlaceholders(XWPFDocument document, Report report) {
+        final String[] volumesHeader = { StringManager.string("header.language"),
+                StringManager.string(HEADER_NUMBER) };
+        final List<String> headerVolumes = new ArrayList<>(Arrays.asList(volumesHeader));
+        final List<List<String>> volumes = DataAdapter.getVolumes(report);
+        DocXTools.fillTable(document, headerVolumes, volumes, VOLUME_TABLE_PLACEHOLDER);
+    }
+
+    /**
+     * Replace quality gate status placeholders with data
+     * 
+     * @param doc    The document containing placeholders
+     * @param report The report containing data
+     */
+    private void replaceQualityGateStatusPlaceholders(XWPFDocument document, Report report) {
+        final String[] qualityGateStatusHeader = { StringManager.string("header.metric"),
+                StringManager.string("header.value") };
+        final List<String> headerQualityGateStatus = new ArrayList<>(Arrays.asList(qualityGateStatusHeader));
+        final List<List<String>> qualityGateStatus = DataAdapter.getQualityGateStatus(report);
+        DocXTools.fillTable(document, headerQualityGateStatus, qualityGateStatus,
+                QUALITY_GATE_STATUS_TABLE_PLACEHOLDER);
+    }
+
+    /**
+     * Replace detailed technical debt placeholders with data
+     * 
+     * @param doc    The document containing placeholders
+     * @param report The report containing data
+     */
+    private void replaceDetailedTechnicalDebtPlaceholders(XWPFDocument document, Report report) {
+        final String[] detailedTechnicalDebtHeader = { StringManager.string("header.reliability"),
+                StringManager.string("header.security"), StringManager.string("header.maintainability"),
+                StringManager.string("header.total") };
+        final List<String> headerDetailedTechnicalDebt = new ArrayList<>(
+                Arrays.asList(detailedTechnicalDebtHeader));
+        final List<List<String>> detailedTechnicalDebt = DataAdapter.getDetailedTechnicalDebt(report);
+        DocXTools.fillTable(document, headerDetailedTechnicalDebt, detailedTechnicalDebt,
+                DETAILED_TECHNICAL_DEBT_TABLE_PLACEHOLDER);
     }
 
 }
