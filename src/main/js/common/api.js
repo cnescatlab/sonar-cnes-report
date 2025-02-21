bc27601d45f7efe20efde507864ca13a1be660a4
Franck Mafféniny <Franck>
2025-02-21 11:29:00 +0100

    FMA - Update compatibility pattern for UI

--- src/main/js/common/api.js
+++ src/main/js/common/api.js
@@ -6,7 +6,7 @@ import { getJSON, postJSON, post } from "sonar-request";
 
 // Function used to get current SonarQube Server version
 export function isCompatible() {
-  const COMPATIBILITY_PATTERN = /(10)(\.\d)(\.\d)*/; // To be change to the SQ 10 LTS
+  const COMPATIBILITY_PATTERN = /(25)(\.\d)(\.\d)*/;
 
   return getJSON("/api/system/status").then(response => {
     return response.version.match(COMPATIBILITY_PATTERN) != null;
